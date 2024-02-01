30b35d9ab84bd83e0ced2e863e419162b47ac0b6
LizzieDworsky <amylizzieroyal@gmail.com>
2024-02-01 14:19:43 -0700

    added car model without user fk

---
+++ backend/models/car.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+//* Schema
+const carSchema = new Schema({
+    // Database level validation
+    make: { type: String, required: true, minlength: 2, maxlength: 255 },
+    model: { type: String, required: true, minlength: 2, maxlength: 255 },
+    year: { type: Number, required: true, min: 1885 },
+});
+
+//* Model
+const Car = model("Car", carSchema);
+
+//* Validation
+const validateCar = (data) => {
+    const schema = Joi.object({
+        // Application level validation
+        make: Joi.string().min(2).max(255).required(),
+        model: Joi.string().min(2).max(255).required(),
+        year: Joi.number().min(1885).required(),
+    });
+    return schema.validate(data);
+};
+
+module.exports = { Car, validateCar };
