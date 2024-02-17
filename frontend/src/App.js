52628437000c221f7eda8309cf1be901bc1e553d
LizzieDworsky <amydworsky15@gmail.com>
2024-02-17 13:36:38 -0700

    implmented route for HomePage, RegisterPage, LoginPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-    return <div className="App">Initialize React App</div>;
+    return (
+        <div className="App">
+            <NavBar /> <Outlet />
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,32 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+
 import { AuthProvider } from "./utils/useAuth";
+import PrivateRoute from "./utils/PrivateRoute";
 
 const router = createBrowserRouter([
     {
         path: "",
         element: <App />,
-        children: [],
+        children: [
+            {
+                path: "/",
+                element: <HomePage />,
+            },
+            {
+                path: "/register",
+                element: <RegisterPage />,
+            },
+            {
+                path: "/login",
+                element: <LoginPage />,
+            },
+        ],
     },
 ]);
 
