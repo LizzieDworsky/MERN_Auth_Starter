fad6bd291609cbb5debe658c59ab855ec190cc5f
LizzieDworsky <amydworsky15@gmail.com>
2024-02-17 14:37:21 -0700

    implmented get all cars by user axios call in loader function + handled error if user does not have cars

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,10 +2,33 @@ import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import axios from "axios";
 
+import { useAuth } from "../utils/useAuth";
+
 export async function getCars() {
-    return null;
+    let token = localStorage.getItem("token");
+    try {
+        let response = await axios.get("http://localhost:5000/api/cars", {
+            headers: { Authorization: "Bearer " + token },
+        });
+        return response.data;
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+            console.warn("Cars not found for this user.", error);
+            return [];
+        } else {
+            throw error;
+        }
+    }
 }
 
 export default function HomePage() {
-    return <div>HomePage</div>;
+    const data = useLoaderData() || [];
+    const [cars, setCars] = useState(data);
+    const { user } = useAuth();
+
+    if (cars.length === 0) {
+        return <div>{`${user.username} currently has no cars.`}</div>;
+    }
+
+    return <div>{`${user.username} currently has cars.`}</div>;
 }
