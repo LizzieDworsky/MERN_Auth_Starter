d0e97853bed79771e720fd44d73bc7abba79f580
LizzieDworsky <amydworsky15@gmail.com>
2024-02-17 14:01:15 -0700

    added registration form, state variable to handle credentials, and handleChange function to handle updates to credentials

--- frontend/src/components/auth/RegisterForm.jsx
+++ frontend/src/components/auth/RegisterForm.jsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+
+import { useAuth } from "../../utils/useAuth";
+
 const RegisterForm = () => {
-    return <div>RegisterForm</div>;
+    const { storeToken } = useAuth();
+    const [credentials, setCredentials] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setCredentials({ ...credentials, [name]: value });
+    };
+
+    return (
+        <div className="auth-form-container">
+            <h1 className="auth-header">Register</h1>
+            <form
+                className="auth-form"
+                // onSubmit={handleSubmit}
+            >
+                <input
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    value={credentials.username}
+                    onChange={handleChange}
+                />
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    value={credentials.email}
+                    onChange={handleChange}
+                />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    value={credentials.password}
+                    onChange={handleChange}
+                />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
 };
 
 export default RegisterForm;
