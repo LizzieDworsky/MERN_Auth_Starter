d2fe45d0ca9569775c9c0f054ed6e4dade9e3af9
LizzieDworsky <amydworsky15@gmail.com>
2024-02-17 14:08:56 -0700

    added handleSubmit function to perform axios post request to the backend with user credentials + useEffect to navigate to HomePage route once user is successfully registered

--- frontend/src/components/auth/RegisterForm.jsx
+++ frontend/src/components/auth/RegisterForm.jsx
@@ -1,47 +1,69 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import { useAuth } from "../../utils/useAuth";
+import { useNavigate } from "react-router-dom";
 
 const RegisterForm = () => {
-    const { storeToken } = useAuth();
+    const navigate = useNavigate();
+    const { storeToken, isAuthenticated } = useAuth();
     const [credentials, setCredentials] = useState({
         username: "",
         email: "",
         password: "",
     });
 
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate("/");
+        }
+    }, [isAuthenticated, navigate]);
+
     const handleChange = (e) => {
         const { name, value } = e.target;
         setCredentials({ ...credentials, [name]: value });
     };
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post(
+                "http://localhost:5000/api/auth/register",
+                credentials
+            );
+            const token = response.headers["x-auth-token"];
+            if (token) {
+                storeToken(token);
+            }
+        } catch (error) {
+            console.error("Registration failed:", error);
+        }
+    };
+
     return (
         <div className="auth-form-container">
             <h1 className="auth-header">Register</h1>
-            <form
-                className="auth-form"
-                // onSubmit={handleSubmit}
-            >
+            <form className="auth-form" onSubmit={(e) => handleSubmit(e)}>
                 <input
                     name="username"
                     type="text"
                     placeholder="Username"
                     value={credentials.username}
-                    onChange={handleChange}
+                    onChange={(e) => handleChange(e)}
                 />
                 <input
                     name="email"
                     type="email"
                     placeholder="Email"
                     value={credentials.email}
-                    onChange={handleChange}
+                    onChange={(e) => handleChange(e)}
                 />
                 <input
                     name="password"
                     type="password"
                     placeholder="Password"
                     value={credentials.password}
-                    onChange={handleChange}
+                    onChange={(e) => handleChange(e)}
                 />
                 <button type="submit">Register</button>
             </form>
