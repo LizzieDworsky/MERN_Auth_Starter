4625578038ebdd837dc92b62c3ad0bc3655f8d61
LizzieDworsky <amydworsky15@gmail.com>
2024-02-17 14:12:36 -0700

    added login form, state variable to handle credentials, and handleChange function to handle updates to credentials

--- frontend/src/components/auth/LoginForm.jsx
+++ frontend/src/components/auth/LoginForm.jsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import { useAuth } from "../../utils/useAuth";
+
 const LoginForm = () => {
-    return <div>LoginForm</div>;
+    const { storeToken, isAuthenticated } = useAuth();
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setCredentials({ ...credentials, [name]: value });
+    };
+
+    return (
+        <div className="auth-form-container">
+            <h1 className="auth-header">Login</h1>
+            <form className="auth-form">
+                <input
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    value={credentials.username}
+                    onChange={(e) => handleChange(e)}
+                />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    value={credentials.password}
+                    onChange={(e) => handleChange(e)}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 };
 
 export default LoginForm;
