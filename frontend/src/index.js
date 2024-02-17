c4008538e3e107f0769dc71850a880bd58463411
LizzieDworsky <amydworsky15@gmail.com>
2024-02-17 13:16:33 -0700

    installed dependancies + implmented base for React Router in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App";
+import { AuthProvider } from "./utils/useAuth";
+
+const router = createBrowserRouter([
+    {
+        path: "",
+        element: <App />,
+        children: [],
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
     </React.StrictMode>
 );
